20ba18c5cd07e16b7bc2e810f2935e5e818910ee
pom <rabbit_liujch@163.com>
2014-08-08 14:16:42 +0800

    test
    test

---
+++ i2huo-parent/i2huo-service/src/main/java/org/i2huo/service/App.java
@@ -0,0 +1,13 @@
+package org.i2huo.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
